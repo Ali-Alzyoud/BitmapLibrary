21321a74103b0e63b5cac3109ae01182b2add883
Ali <Ali Alzyoud>
2021-09-07 10:06:45 +0300

    remove tabs

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -5,20 +5,38 @@
 
 #define DATA this->_data
 
-unsigned int colorSpaceSize(COLOR_SPACE colorspace) {
-	switch (colorspace)
-	{
-	case COLOR_SPACE_RGB24:
-		return 24;
-	default:
-		return 0;
-	}
+unsigned int colorSpaceSize(COLOR_SPACE colorspace)
+{
+    switch (colorspace)
+    {
+    case COLOR_SPACE_RGB24:
+        return 24;
+    default:
+        return 0;
+    }
 }
-
 
 Bitmap::Bitmap(unsigned int width, unsigned int height, COLOR_SPACE colorspace)
 {
-	unsigned dataSize = width * height * colorSpaceSize(colorspace);
-	DATA = (unsigned char *)malloc(dataSize);
-	memset(DATA, 0, dataSize);
+    unsigned dataSize = width * height * colorSpaceSize(colorspace);
+    DATA = (unsigned char *)malloc(dataSize);
+    memset(DATA, 0, dataSize);
+}
+
+Pixel Bitmap::getPixel(UINT x, UINT y) const
+{
+    Pixel pixel={0};
+    if (x >= this->_width || y >= this->_height) return pixel;
+
+    //ali.m needs to add implementation;
+
+}
+
+UINT Bitmap::getWidth() const
+{
+    return this->_width;
+}
+UINT Bitmap::getHeight() const
+{
+    return this->_height;
 }
--- Bitmap.h
+++ Bitmap.h
@@ -1,22 +1,33 @@
-#pragma once
-enum COLOR_SPACE {
-	COLOR_SPACE_RGB24 = 0
+#ifndef BITMAP_H
+#define BITMAP_H
+enum COLOR_SPACE
+{
+    COLOR_SPACE_RGB24 = 0
 };
 
 typedef unsigned int UINT;
 typedef unsigned char BYTE;
 
+struct Pixel{
+    BYTE r,g,b;
+};
+
+
+
 class Bitmap
 {
 public:
-	Bitmap(UINT width, UINT height, COLOR_SPACE colorspace);
-	BYTE* getPixel();
-	void setPixel(BYTE* data);
-	BYTE* getRow(UINT row, UINT size);
-	void setRow(UINT row, BYTE *data, UINT size);
-private:
-	UINT _width;
-	UINT _height;
-	unsigned char *_data;
-};
+    Bitmap(UINT width, UINT height, COLOR_SPACE colorspace);
+    Pixel getPixel(UINT x, UINT y) const;
+    void setPixel(Pixel);
+    const BYTE *getRow(UINT row, UINT size) const;
+    void setRow(UINT row, BYTE *data, UINT size);
+    UINT getWidth() const;
+    UINT getHeight() const;
 
+private:
+    UINT _width;
+    UINT _height;
+    unsigned char *_data;
+};
+#endif//#ifndef BITMAP_H
